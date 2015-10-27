2c42e02cdf15d9fb25ddb2965c88986acd52b0d2
Kiryl Korzun <megaflex2012@gmail.com>
2015-10-27 20:18:44 +0300

    Create IAge.cs

---
+++ IAge.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mix_in
+{
+    public interface IAge
+    {
+        int Age { get; set; }
+        void OtherAge();
+    }
+}
